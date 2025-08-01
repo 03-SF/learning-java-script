c67d60068af3ab8a6bf994124110effa4ee74a40
Sumrun <sumrunfatima57@gmail.com>
2025-08-01 17:27:57 +0000

    config done

---
+++ 01_BASICS/04_operations.js
@@ -0,0 +1,17 @@
+let value = 3
+let negValue = -value
+console.log(negValue) //output = -3
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**3) -> equals 2 powered 3
+//console.log(2/3)
+//console.log(2%3) -> gives remainder/modulus
+
+let str1 = "hello"
+let str2 = " sumrun"
+let str3 = str1 + str2
+
+console.log(str3) 
+
