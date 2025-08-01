a84898a6c70336d01ab14d24ba2ecfc8b549b7c6
Sumrun <sumrunfatima57@gmail.com>
2025-08-01 17:50:34 +0000

    config done

--- 01_BASICS/04_operations.js
+++ 01_BASICS/04_operations.js
@@ -15,3 +15,20 @@ let str3 = str1 + str2
 
 console.log(str3) 
 
+
+console.log(1+"2"); //output:12
+console.log("2"+1); //output:21
+console.log("1"+2+2); //output: 122
+console.log(1+2+"2"); //output:32
+
+//console.log(+true);  //avpid this, output is error
+console.log(+""); //avpid this, output is 0
+
+//focus on readibility 
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+--gameCounter;
+console.log(gameCounter);
