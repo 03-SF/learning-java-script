5969e0c371669f582b3d065a9cb0929c7c1e8494
Sumrun <sumrunfatima57@gmail.com>
2025-08-03 17:48:40 +0000

    config done

---
+++ 08_strings.js
@@ -0,0 +1,7 @@
+const name= "sumrun"
+const repCount = 50
+
+//console.log(name +repCount + " value")
+//better way of writing:
+console.log(`Hello may name is ${name} and my repo count is ${repCount}`)
+
