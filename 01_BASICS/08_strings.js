fe610fe1c689affd8b35c41b12f4e674d4d5843d
Sumrun <sumrunfatima57@gmail.com>
2025-08-04 18:44:15 +0000

    config done

--- 01_BASICS/08_strings.js
+++ 01_BASICS/08_strings.js
@@ -5,3 +5,12 @@ const repCount = 50
 //better way of writing:
 console.log(`Hello may name is ${name} and my repo count is ${repCount}`)
 
+//declaring string is an object
+//declaring string:
+const gameName = new String('sumrun')
+
+console.log(gameName.__proto__)
+
+console.log(gameName.toUpperCase())
+//checking whcih position a certain character is at: 
+console
